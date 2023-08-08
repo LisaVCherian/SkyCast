82db3396d29b8a2004865a1c4c6a062c0ad6d547
LisaVCherian <lisaviju02@gmail.com>
2023-08-08 15:06:29 +0530

    daily forecast added

--- src/App.js
+++ src/App.js
@@ -4,26 +4,65 @@ import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation.jsx'
 import Temperature from './components/Temperature';
 import Forecast from './components/Forecast';
-import getWeatherData from './services/weatherService';
+import getFormattedWeatherData from './services/weatherService';
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
 
-  const fetchWeather = async () => {
-      const data = await getWeatherData('weather', {q:"london"});
-      console.log(data);
-  }
+  // const [query, setQuery] = useState({q:'berlin'})
+  // const [units, setUnits] = useState('metric')
+  // const [weather, setWeather] = useState(null)
 
-  fetchWeather();
+  // useEffect(() => {
+  //   const fetchWeather = async () => {
+  //     const message = query.q ? query.q : "current location.";
+
+  //     toast.info("Fetching weather for " + message);
+
+  //     await getFormattedWeatherData({ ...query, units }).then((data) => {
+  //       toast.success(
+  //         `Successfully fetched weather for ${data.name}, ${data.country}.`
+  //       );
+
+  //       setWeather(data);
+  //     });
+  //   };
+
+  //   fetchWeather();
+  // }, [query, units]);
+
+  // const formatBackground = () => {
+  //   if (!weather) return "from-cyan-700 to-blue-700";
+  //   const threshold = units === "metric" ? 20 : 60;
+  //   if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+  //   return "from-yellow-700 to-orange-700";
+  // };
 
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons/>
-      <Inputs/>
-      <TimeAndLocation/>
-      <Temperature/>
-      <Forecast title="hourly forecast"/>
-      <Forecast title="daily forecast"/>
-    </div>
+  //   <div>
+  //   {weather && (
+  //     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+  //     <TopButtons/>
+  //     <Inputs/>
+  //     <TimeAndLocation weather={weather}/>
+  //     <Temperature weather={weather}/>
+  //     <Forecast title="hourly forecast"/>
+  //     <Forecast title="daily forecast"/>
+  //   </div>
+  // )}
+
+  // </div>
+
+  <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+       <TopButtons/>
+       <Inputs/>
+       <TimeAndLocation/>
+       <Temperature/>
+       <Forecast title="hourly forecast"/>
+       <Forecast title="daily forecast"/>
+     </div>
   );
 }
 
